8a4281897cd9826b3bcc3713110056ba8f6dde4b
girish-fuluskar <girish.fuluskar@gmail.com>
2016-10-19 21:16:51 +0530

    platform specific (ios or android) document folder access code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', ['base64'])
+angular.module('app.controllers', ['base64','ionic'])
   
 .controller('dashboardCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
